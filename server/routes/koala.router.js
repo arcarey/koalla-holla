9918de57d0819bf0809c5f67fd42731db75d3050
Andrew Carey <andrew.r.carey@gmail.com>
2022-11-10 14:21:33 -0600

    fixed table name in database.sql, configured db pool, tested with GET request of full table

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,10 +2,27 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool ({
+    database: 'koala-holla',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
 
 // GET
-
+koalaRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM holla_table;';
+    pool.query(queryText).then((result) => {
+        console.log(result.rows);
+        res.send(result.rows);
+    }).catch ((err) => {
+        console.log('Error making query:', queryText, 'error:', err);
+        req.sendStatus(500);
+    })
+})
 
 // POST
 
